ce7937c13457fddfe0e4fb0f98ffb5d547c30fbb
danil645 <acc2234jetbrains@mail.ru>
2023-07-17 16:41:07 +0700

    fix adding form results to admin panel

--- local/templates/tasks_site/components/bitrix/form.result.new/contact/template.php
+++ local/templates/tasks_site/components/bitrix/form.result.new/contact/template.php
@@ -1,53 +1,42 @@
-<?
+<?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? endif; ?>
+if ($arResult["isFormErrors"] == "Y"): ?>
+    <?= $arResult["FORM_ERRORS_TEXT"]; ?>
+<?php endif; ?>
+
 <?= $arResult["FORM_NOTE"] ?>
 
-<? if ($arResult["isFormNote"] != "Y") {
-    ?>
-    <!--                               Replacing the class name-->
-    <? $arResult["FORM_HEADER"] = preg_replace("#<form#", "<form class='contact-form__form'", $arResult["FORM_HEADER"]); ?>
+<?php if ($arResult["isFormNote"] != "Y") { ?>
+    <!-- Replacing the class name -->
+    <?php $arResult["FORM_HEADER"] = preg_replace("#<form#", "<form class='contact-form__form'", $arResult["FORM_HEADER"]); ?>
     <?= $arResult["FORM_HEADER"] ?>
     <div class="contact-form">
         <div class="contact-form__head">
-            <?
-            if ($arResult["isFormDescription"] == "Y" || $arResult["isFormTitle"] == "Y" || $arResult["isFormImage"] == "Y") {
-                ?>
-                <?
-                if ($arResult["isFormTitle"]) {
-                    ?>
+            <?php if ($arResult["isFormDescription"] == "Y" || $arResult["isFormTitle"] == "Y" || $arResult["isFormImage"] == "Y") { ?>
+                <?php if ($arResult["isFormTitle"]) { ?>
                     <div class="contact-form__head-title"><?= $arResult["FORM_TITLE"] ?></div>
-                    <?
-                } //endif ;
-                ?>
+                <?php } ?>
                 <div class="contact-form__head-text"><?= $arResult["FORM_DESCRIPTION"] ?></div>
-                <?
-            } // endif
-            ?>
+            <?php } ?>
         </div>
 
         <form class="contact-form__form" action="/" method="POST">
             <div class="contact-form__form-inputs">
-                <?
-                foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
+                <?php foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
                     if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] == 'hidden') {
                         echo $arQuestion["HTML_CODE"];
-                    } else {
-                        ?>
+                    } else { ?>
                         <div class="input contact-form__input">
                             <label class="input__label">
                                 <div class="input__label-text">
-                                    <? if (is_array($arResult["FORM_ERRORS"]) && array_key_exists($FIELD_SID, $arResult['FORM_ERRORS'])): ?>
+                                    <?php if (is_array($arResult["FORM_ERRORS"]) && array_key_exists($FIELD_SID, $arResult['FORM_ERRORS'])): ?>
                                         <span class="error-fld"
                                               title="<?= htmlspecialcharsbx($arResult["FORM_ERRORS"][$FIELD_SID]) ?>"></span>
-                                    <? endif; ?>
-                                    <?= $arQuestion["CAPTION"] ?><? if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><? endif;
-                                    ?></div>
-
-<!--                               Replacing the class name-->
-                                <?
-
+                                    <?php endif; ?>
+                                    <?= $arQuestion["CAPTION"] ?><?php if ($arQuestion["REQUIRED"] == "Y"): ?><?= $arResult["REQUIRED_SIGN"]; ?><?php endif; ?>
+                                </div>
+                                <?php
                                 $arQuestion["HTML_CODE"] = preg_replace('#class="inputtext"#',
                                     "class='input__input'", $arQuestion["HTML_CODE"]);
                                 $arQuestion["HTML_CODE"] = preg_replace('#textarea#',
@@ -56,17 +45,16 @@ if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? 
                                 <?= $arQuestion["HTML_CODE"] ?>
                             </label>
                         </div>
-                        <?
-                    }
-                } //endwhile
-                ?>
+                    <?php }
+                } ?>
             </div>
-
+            <input <?= (intval($arResult["F_RIGHT"]) < 10 ? "disabled=\"disabled\"" : ""); ?> type="hidden"
+                                                                                              name="web_form_submit"
+                                                                                              value="<?= htmlspecialcharsbx(trim($arResult["arForm"]["BUTTON"]) == '' ? GetMessage("FORM_ADD") : $arResult["arForm"]["BUTTON"]); ?>"/>
             <div class="contact-form__bottom">
                 <div class="contact-form__bottom-policy">Нажимая &laquo;Отправить&raquo;, Вы&nbsp;подтверждаете, что
                     ознакомлены, полностью согласны и&nbsp;принимаете условия &laquo;Согласия на&nbsp;обработку
-                    персональных
-                    данных&raquo;.
+                    персональных данных&raquo;.
                 </div>
                 <button class="form-button contact-form__bottom-button" data-success="Отправлено"
                         data-error="Ошибка отправки">
@@ -76,5 +64,4 @@ if ($arResult["isFormErrors"] == "Y"):?><?= $arResult["FORM_ERRORS_TEXT"]; ?><? 
         </form>
     </div>
     <?= $arResult["FORM_FOOTER"] ?>
-    <?
-} //endif (isFormNote)
+<?php } //endif (isFormNote) ?>
